cd88c630d25b70c1dc715fbcb3a1c5f52898a0bc
ThiagoDomcoski <thiago84d@gmail.com>
2022-06-06 14:57:40 -0300

    update to parceiros

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const empresas = [
+    {
+        'name':'arautran',
+        'phone':'+5541996211600'
+    },
+    {
+        'name':'piloto',
+        'phone':'+5541999180066'
+    },
+    {
+        'name':'giusoft',
+        'phone':'+5571992339330'
+    }
+];
+
+const p = new URLSearchParams(window.location.search).get('p');
+$(document).ready(function(){
+    if(p != null){
+        var e = empresas.find((e) => e.name == p);
+        if(e != null){
+            $("#img-parceiro").attr("src", "./img/c-"+p+".jpeg");
+            $("#cetci-link").attr("href", 
+            "https://api.whatsapp.com/send?phone="+e.phone+"&text=Olá, gostaria de mais informações sobre o curso *CETCI - Condutor Especializado em Transporte de Cargas Indivisíveis*"
+            );
+            $("#cetpp-link").attr("href", 
+            "https://api.whatsapp.com/send?phone="+e.phone+"&text=Olá, gostaria de mais informações sobre o curso *CETPP - Condutor Especializado em Transporte de Produtos Perigosos - MOPP*"
+            );
+            $("#cete-link").attr("href", 
+            "https://api.whatsapp.com/send?phone="+e.phone+"&text=Olá, gostaria de mais informações sobre o curso *CETE - Condutor Especializado em Transporte de Escolares*"
+            );
+            $("#cetcp-link").attr("href", 
+            "https://api.whatsapp.com/send?phone="+e.phone+"&text=Olá, gostaria de mais informações sobre o curso *CETCP - Condutor Especialozado em Transporte Coletivo de Passageiros*"
+            );
+            $("#cetve-link").attr("href", 
+            "https://api.whatsapp.com/send?phone="+e.phone+"&text=Olá, gostaria de mais informações sobre o curso *CETVE - Condutor Especializado em Transporte de Veículos de Emergência*"
+            );
+        }
+        
+    }
+});
