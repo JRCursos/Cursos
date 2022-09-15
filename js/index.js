f109be0dd37cbe42e3aac7b24015e5a12138883c
ThiagoDomcoski <thiago84d@gmail.com>
2022-09-15 16:38:12 -0300

    json

--- js/index.js
+++ js/index.js
@@ -180,7 +180,6 @@ const empresas = [
     {'name':'ArcoIris', 'phone':'+554332622779', 'uf':'PR'},
     {'name':'SS', 'phone':'+5543991532605', 'uf':'PR'},
     {'name':'Inova', 'phone':'+554330511414', 'uf':'PR'},
-
     {'name':'Parana', 'phone':'+554333233030', 'uf':'PR'},
-    
+    {'name':'Shalon', 'phone':'+5543988025429', 'uf':'PR'}
 ];
